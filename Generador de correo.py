3c3ed83d684f28ffdb9932b473503d784d09de9b
Eduardo Torruella <eduardo.torruella@keyinstitute.edu.sv>
2025-05-16 15:15:18 -0600

    Ejercicio 1

---
+++ Generador de correo.py
@@ -0,0 +1,13 @@
+#Correo Key
+#Clase:        Clase 1
+#Tema:         Fundamentos
+#Ejercicio:    Identificador de la clase
+#Descripción:  Definición del ejercicio
+#Autor:        Eduardo Torruella
+#Fecha:        2025-05-14
+#Estado:       [ Terminado ]
+
+nombre = input("Ingresa tu nombre: ")
+apellido = input ("Ingresa tu primer apellido: ")
+
+print (f"{nombre.lower()}.{apellido.lower()}@keyinstitute.edu.sv")
