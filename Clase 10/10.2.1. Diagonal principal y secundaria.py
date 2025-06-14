74de65ee326d82924f0f2481e7e8e5c3adb21542
Eduardo Torruella <eduardo.torruella@keyinstitute.edu.sv>
2025-06-14 12:45:30 -0600

    Clase 10

---
+++ Clase 10/10.2.1. Diagonal principal y secundaria.py
@@ -0,0 +1,27 @@
+#Clase:        Clase 10
+#Tema:         Manejo de matrices
+#Ejercicio:    10.2.1. Diagonal principal y secundaria
+#Descripción:  Es un programa que solicita al usuario ingresar los datos para crear una matriz, recorre la matriz e imprime la diagonal principal (izquierda a derecha) y la diagonal secundaria (de derecha a izquierda).
+#Fecha:        2025-06-09
+#Estado:       [ Terminado ]
+
+N = int(input())
+l =  []
+for i in range(N):
+    temp = list(map(int, input().split()))
+    l.append(temp)
+
+
+dp = []
+ds =[]
+
+for i in range(len(l)):
+    for j in range(len(l[i])):
+        if i == j:
+            dp.append(l[i][j])
+            
+        if i + j == len(l)-1:
+            ds.append(l[i][j])
+
+print (dp)
+print (ds)
---
+++ Clase 10/10.3.1. Matriz simétrica.py
@@ -0,0 +1,34 @@
+#Clase:        Clase 10
+#Tema:         Manejo de matrices
+#Ejercicio:    10.3.1. Matriz simétrica
+#Descripción:  Es un programa que solicita al usuario una matriz, la recorre y enceuntra su diagonal principal, luego verifica e imprime si es una matriz simétrica o no.
+#Fecha:        2025-06-09
+#Estado:       [ Terminado ]
+
+N = int(input())
+m =  []
+for i in range(N):
+    temp = list(map(int, input().split()))
+    m.append(temp)
+
+diagonal_p = []
+
+for i in range(len(m)):
+    for j in range(len(m[i])):
+        if i == j:
+            diagonal_p.append(m[i][j])
+
+simetria = True
+for f in range(len(m)):
+    for c in range(len(m[f])):
+        if m[f][c] == m[c][f]:
+            continue
+        else:
+            simetria = False
+            break 
+
+if simetria:
+    print ("La amtriz es simétrica")
+else:
+    print ("La amtriz no es simétrica")
+
